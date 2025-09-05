d90905b1791c28d93fd1379057848eee9ae90260
jeziorjp-cmyk <jeziorjp@clarkson.edu>
2025-09-05 12:56:42 -0400

    Update ma211_hw1.lean

--- ma211_hw1.lean
+++ ma211_hw1.lean
@@ -4,7 +4,7 @@ import AutograderLib
 
 math2001_init
 
-theorem prob1 {u v w : ℝ} (h1 : u + 5 = v + 4) (h2 : v + 2 = w + 5) : u = w + 2 := by
+theorem prob1 {u v w : ℝ} (h1 : u + 5 = v + 4) (h2 : v + 2 = w + 5) : u = w + 2 := 
   calc
     u = u + 5 - 5 := by ring
     _ = v + 4 - 5 := by rw [h1]
@@ -13,7 +13,8 @@ theorem prob1 {u v w : ℝ} (h1 : u + 5 = v + 4) (h2 : v + 2 = w + 5) : u = w + 
     _ = w + 2 := by ring
 
 theorem prob2 {x y : ℝ} (h1 : 2 * x + 3 * y = 12) (h2 : 2 * y + 3 * x = 13) : x = 3 := by
-  sorry
+  calc
+    
 
 theorem prob3 {a b c : ℚ} (h1 : a < b) (h2 : b < c) : a < (a + b + c) / 3 := by
   sorry
